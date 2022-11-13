ddf36fe8fba3f44b2c59065db15f0fcf63bd7639
Petr Chatrný <56938867+petrchatrny@users.noreply.github.com>
2022-11-14 00:04:00 +0100

    add api blueprint

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from src.routes.api import api_blueprint
 
 # initialize application
 app = Flask(__name__)
@@ -11,6 +12,9 @@ if environ == "production":
     app.config.from_object("src.config.ProductionConfig")
 else:
     app.config.from_object("src.config.DevelopmentConfig")
+
+# register blueprints
+app.register_blueprint(api_blueprint)
 
 if __name__ == "__main__":
     app.run()
