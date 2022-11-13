913b6fbfb7d4c36d792963ebefcbdd5562c14bdf
Petr Chatrný <56938867+petrchatrny@users.noreply.github.com>
2022-11-13 23:00:15 +0100

    add config.py

--- src/app.py
+++ src/app.py
@@ -1,12 +1,16 @@
+import os
+
 from flask import Flask
 
+# initialize application
 app = Flask(__name__)
 
-
-@app.route("/")
-def hello_world():
-    return "<font size='30'><marquee scrollamount='12' direction='right'>Hello World!</marquee></font>"
-
+# app configuration
+environ = os.environ.get("FLASK_ENV")
+if environ == "production":
+    app.config.from_object("src.config.ProductionConfig")
+else:
+    app.config.from_object("src.config.DevelopmentConfig")
 
 if __name__ == "__main__":
     app.run()
