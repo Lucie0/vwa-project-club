d15195a60715bffe547e3b2527e0614c8a15c0ea
Petr Chatrný <56938867+petrchatrny@users.noreply.github.com>
2022-11-14 02:09:15 +0100

    create raw main app

---
+++ src/static/js/main.js
@@ -0,0 +1,7 @@
+function toggleNav() {
+    const nav = document.getElementById("nav");
+    const burger = document.getElementById("burger");
+
+    nav.classList.toggle("nav-active");
+    burger.classList.toggle("burger-active");
+}
